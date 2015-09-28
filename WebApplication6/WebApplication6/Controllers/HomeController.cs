0500a8311df055f3cd887157c916a76d7f667d36
Martina Magnusson <magnusson.martina@hotmail.com>
2015-09-28 09:42:54 +0200

    knockout

---
+++ WebApplication6/WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Numbers numbers = new Numbers();
+            
+            return View(numbers);
+        }
+    }
+}
---
+++ WebApplication6/WebApplication6/Models/Numbers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace WebApplication6.Models
+{
+    public class ColoredNumber
+    {
+        public int value;
+        public string color;
+
+        public ColoredNumber(int value)
+        {
+            this.value = value;
+        }
+    }
+    public class RandomNumbers
+    {
+        private Random _randomNumber;
+        public int[] numbers {
+            get {
+                return new int[] { _randomNumber.Next(0,100), _randomNumber.Next(0, 100), _randomNumber.Next(0, 100), _randomNumber.Next(0, 100), _randomNumber.Next(0, 100), _randomNumber.Next(0, 100) };
+            }
+        }
+        public RandomNumbers()
+        {
+            _randomNumber = new Random();
+            Thread.Sleep(100);
+        }  
+    }
+
+    public class StaticNumbers
+    {
+        private Random _randomStaticNumber;
+        public List<int> numbers
+        {
+            get
+            {
+                return new List<int>() {};
+            }
+        }
+        public StaticNumbers()
+        {
+            _randomStaticNumber = new Random();
+        }
+    }
+
+    public class Numbers
+    {
+        public RandomNumbers randomNumbers = new RandomNumbers();
+        public StaticNumbers staticNumbers = new StaticNumbers();   
+        public ColoredNumber color { get; set; }
+    }
+}   
